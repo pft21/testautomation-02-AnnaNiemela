f3b7906ab0246f1cb10fa9a7a98fdaa36ea2d34b
Anna Niemelä <anna.niemela@studerande.plushogskolan.se>
2021-05-04 11:46:05 +0200

    First commit

---
+++ front-end/cypress/integration/assignment03.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Test suite with Page-objects - Assignment02', () =>{
+
+    beforeEach ('Visit website and log in', () => {
+        
+    })
+
+    afterEach ('Log out', () => {
+        
+    })
+
+    it('', () => {
+        
+    })
+
+
+})
